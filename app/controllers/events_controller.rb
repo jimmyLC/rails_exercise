68fc6ce314660f96a731e7867921b394f449b017
Jimmy <acsd68500@gmail.com>
2014-10-23 12:24:49 +0800

    refactor controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,11 @@
 class EventsController < ApplicationController
+	before_action :find_params, :only => [:show, :edit, :update, :destroy]
 
 	def index
-		@events = Event.all
+		@events = Event.page(params[:page]).per(5)
 	end
 
 	def show
-		@event = Event.find(params[:id])
 	end
 
 	def new
@@ -14,29 +14,40 @@ class EventsController < ApplicationController
 
 	def create
 		@event = Event.new(event_params)
-		@event.save
 
-		redirect_to :action => :index
+		if @event.save
+			flash[:notice] = "event was successfully created"
+			redirect_to :action => :index
+		else
+			render :action => :new
+		end
+		
 	end
 
 	def edit
-		@event = Event.find(params[:id])
 	end
 
 	def update
-		@event = Event.find(params[:id])
-		@event.update(event_params)
-
-		redirect_to :action => :show, :id => @event
+		if @event.update(event_params)
+			flash[:notice] = "event was successfully updated"
+			redirect_to :action => :show, :id => @event
+		else
+			render :action => :edit
+		end
 	end
 
 	def destroy
-		@event = Event.find(params[:id])
 		@event.destroy
+		flash[:alert] = "event was successfully deleted"
 		redirect_to :action => :index
 	end
 
 	private
+
+	def  find_params
+		@event = Event.find(params[:id])
+	end
+
 	def event_params
 		params.require(:event).permit(:name, :description)
 	end
