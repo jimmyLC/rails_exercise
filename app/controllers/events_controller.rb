c50cdfaf9c17ccf20a3654ee0f087ec8c962bbfb
Jimmy <acsd68500@gmail.com>
2014-10-23 10:51:02 +0800

    strong params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,19 @@ class EventsController < ApplicationController
 		@events = Event.all
 	end
 
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		@event.save
+
+		redirect_to :action => :index
+	end
+
+	private
+	def event_params
+		params.require(:event).permit(:name, :descrition)
+	end
 end
